e98732dbb8431a120f9fcb2fa924cd581053b3e5
Ludo3264 <onlineludo@gmail.com>
2026-08-28 14:25:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ SOURCES = {
     "Santé": {
         "Le Monde Sciences": "https://www.lemonde.fr/sciences/recherche/?search_keywords=",
         "INSERM": "https://www.inserm.fr/?s=",
-        "ANSES": "https://www.anses.fr/fr/search",
+        "ANSES": "https://www.anses.fr/fr/recherche/site?keys=",
         "OMS": "https://www.who.int/fr/search/?q=",
         "Vidal (médicaments)": "https://www.vidal.fr/recherche/?q=",
     },
